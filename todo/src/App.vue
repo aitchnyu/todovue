0b4abb6aaabeea5e97ddc91fd695873bdfa2221a
Jesvin Jose <frank.einstien@gmail.com>
2022-07-07 19:59:19 +0530

    Started blank app with Vue and Beufy

---
+++ todo/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <todo-app />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import TodoApp from "./components/TodoApp.vue";
+
+export default {
+  name: "App",
+  components: {
+    TodoApp,
+  },
+};
+</script>
---
+++ todo/src/components/TodoApp.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="hello">todo</div>
+</template>
+
+<script>
+export default {
+  name: "TodoApp",
+  props: {
+    // msg: String,
+  },
+};
+</script>
